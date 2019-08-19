1a0dddf746d6f64861d8d3bb1feb00ef8a80b4eb
mohdraz <noorinfotechinc@gmail.com>
2019-08-18 19:51:32 -0500

    project submission

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,69 @@
 // CODE here for your Lambda Classes
+
+class Person {
+    constructor(personAttrs) {
+        this.name = personAttrs.name;
+        this.age = personAttrs.age;
+        this.location = personAttrs.location;
+    }
+
+    speak() {
+        return `Hello my name is ${this.name}, I am from ${this.location}.`;
+    }
+}
+
+class Instructor extends Person {
+    constructor(instructorAttrs) {
+        super(instructorAttrs);
+        this.speciality = instructorAttrs.speciality;
+        this.favLanguage = instructorAttrs.favLanguage;
+        this.catchPhrase = instructorAttrs.catchPhrase;
+    }
+
+    demo(subject) {
+        return `Today we are learning about ${subject}`;
+    }
+
+    grade(student, subject) {
+        return `${student.name} receives a perfect score on ${subject}`;
+    }
+}
+
+class Student extends Person {
+    constructor(studentAttrs) {
+        super(studentAttrs);
+        this.previousBackground = studentAttrs.previousBackground;
+        this.className = studentAttrs.className;
+        this.favSubjects = studentAttrs.favSubjects;
+    }
+    listsSubjects() {
+        this.favSubjects.map(item => {
+            console.log(item);
+        }) 
+    }
+
+    PRAssignment(subject) {
+        return `${student.name} has submitted a PR for ${subject}`;
+    }
+
+    sprintChallenge(subject) {
+        return `${student.name} has begun sprint challenge on ${subject}`;
+    }
+}
+
+class PojrectManagers extends Instructor{
+    constructor(pmAttrs) {
+        super(pmAttrs);
+        this.gradClassName = pmAttrs.gradClassName;
+        this.favInstructor = pmAttrs.favInstructor;
+    }
+
+    standUP(channel) {
+        return `${this.name} announces to ${channel}`;
+    }
+
+    debugCode(student, subject) {
+        return `${this.name} debugs ${student.name}'s code on ${subject}`;
+    }
+}
+
